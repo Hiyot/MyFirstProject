806d9e0ca8dc81ebcbf76913e7170aed4fb7d8bb
Hiyot <hiyot95@gmail.com>
2023-01-19 21:14:16 +0200

    new if

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to currncy convert");
+        System.out.println("Please choose an option (1/2):");
 
         int num = 0;
         try {
             Scanner scan = new Scanner(System.in);
             num = scan.nextInt();
-            System.out.println("Please choose an option (1/2):");
             System.out.println("1.Dollar to Shekel");
             System.out.println("2.Shekel to Dollar");
 
@@ -18,6 +18,13 @@ public class Main {
             System.out.println(num);
         } catch (ArrayIndexOutOfBoundsException | InputMismatchException e){
             System.out.println("invalid choise please print again");
+        }finally {
+        }
+
+        while (num != 2 && num != 1){
+            System.out.println("invalid Choice, please try again");
+            Scanner scan2 = new Scanner(System.in);
+            num = scan2.nextInt();
         }
 
         Coins coins = Coins.values()[num - 1];
@@ -29,7 +36,23 @@ public class Main {
 
         }
 
+        System.out.println("please enter an amount to convert");
 
+        double number = 0;
+
+       try{ Scanner scan3 = new Scanner(System.in);
+        number = scan3.nextDouble();}
+       catch (ArrayIndexOutOfBoundsException | InputMismatchException e){
+        }finally {
+       }
+
+
+       double[] myList={0.28, 3.52};
+
+       for (double i = 0; i <myList.length; i++){
+           if (i!=0.28 && i!=3.52);
+           System.out.println();
+       }
 
     }
 
